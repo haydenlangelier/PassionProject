a130d53cc0cc7f7c52207748b6f7e1c3662e7f5e
Hayden Langelier <haydenlangelier@utexas.edu>
2016-08-19 11:20:44 -0700

    seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,8 @@
-User.create(email: 'bobbysags@gmail.com', name: 'Bob Saget', password: '123')
+User.create(email: 'millie@gmail.com', name: 'millie', password: '123')
+User.create(email: 'hayden@gmail.com', name: 'hayden', password: '123')
+User.create(email: 'david@gmail.com', name: 'david', password: '123')
+User.create(email: 'parth@gmail.com', name: 'parth', password: '123')
+User.create(email: 'sarah@gmail.com', name: 'sarah', password: '123')
+User.create(email: 'armon@gmail.com', name: 'armon', password: '123')
+
+
